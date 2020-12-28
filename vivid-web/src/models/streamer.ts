8c923f6665a5389187e77858f77e229c5401f8f1
ctnimmo <ctnimmo@yahoo.co.uk>
2020-12-28 18:51:22 +0000

    Initial Commit - Part 8 - vivid-web src models

---
+++ vivid-web/src/models/streamer.ts
@@ -0,0 +1,8 @@
+export interface Streamer {
+    StreamerHeaderID: string;
+    StreamerNumber: string;
+    MadeFor: string;
+    DueDate: string;
+    Status: string;
+}
+export interface StreamerItems { }
